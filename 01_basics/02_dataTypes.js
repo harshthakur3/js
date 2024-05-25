9bdcf050df89887b6f7d74a4765aa1c0ffebdef8
Harsh Thakur <154436704+harshthakur3@users.noreply.github.com>
2024-05-25 13:13:32 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treal all js code as newer version
+
+//alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3);
+
+let name = "harsh"
+let age = 18
+let isLoggedIn = false
+
+// numbers => 2 to power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof 18);
