9be6620100f0dc8bdc19cff697467af9ec9c9143
Harsh Thakur <154436704+harshthakur3@users.noreply.github.com>
2024-05-25 12:53:41 +0000

    Let, Const and Var ki kahani

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const account_Id = 144553
+let account_Email = "harsh@google.com"
+var account_Password = "12345"
+account_City = "Jamnagar"
+let account_State;
+
+
+/*we can't change value assigned to const keyword
+try to avoid "var" keyword use "let" and "const" insted
+*/
+console.table([account_Id, account_Email, account_Password, account_City, account_State])
