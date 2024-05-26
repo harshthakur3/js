49f460bd9d1f6557a83f2ec7e92a050c47eee526
Harsh Thakur <154436704+harshthakur3@users.noreply.github.com>
2024-05-26 04:53:03 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -13,3 +13,9 @@ console.log(valueInNumber);
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn);
+// ***************operations***************
+
+let str1 = "hello"
+let str2 = " harsh"
+let str3 = str1 + str2
+console.log(str3);
