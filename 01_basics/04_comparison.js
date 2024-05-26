86994561a1d1ad3fc0b5e48b13478b5b3a0a67e8
Harsh Thakur <154436704+harshthakur3@users.noreply.github.com>
2024-05-26 05:05:46 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,10 @@
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+
+// === is called strict check
+
+console.log('2' == 2);
+console.log("2" === 2);
